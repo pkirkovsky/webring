ac59aefd669c1e85e7346caa1a09a24cfc147637
Astrid <astrid@xrtc.net>
2021-01-16 22:08:12 -0800

    add spacegirl

--- sites.js
+++ sites.js
@@ -1,7 +1,12 @@
 let sites = [
-    {
-        name: "æstrid",
-        url: 'https://astrid.smith.seattle.wa.us',
-        tagline: "billing excellence is a full time job"
-    }
+	{
+		name: "æstrid",
+		url: 'https://astrid.smith.seattle.wa.us',
+		tagline: "billing excellence is a full time job"
+	}
+	,{
+		name: "spacegirl",
+		url: "https://witch.spmh.us",
+		tagline: "i have no idea"
+	}
 ]
