deef988a155fea1eb44a84d5adebd7e3581a4e04
AppleDash <derp@derpy.fr>
2021-01-17 14:23:05 -0500

    Add DashTel

--- sites.js
+++ sites.js
@@ -3,10 +3,16 @@ let sites = [
 		name: "æstrid",
 		url: 'https://astrid.smith.seattle.wa.us',
 		tagline: "billing excellence is a full time job"
-	}
-	,{
+	},
+	{
 		name: "spacegirl",
 		url: "https://witch.spmh.us",
 		tagline: "i have no idea"
+	},
+	{
+		name: "DashTel",
+		url: "https://dashtel.ca",
+		tagline: "You've reached DashTel, HatbowlDash speaking, how may I direct your call?"
 	}
+	
 ]
