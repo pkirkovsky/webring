25015fd996027599b26fc2afe244a9ac1448df5f
Astrid <astrid@xrtc.net>
2021-01-18 10:43:39 -0800

    un fuck json

--- sites.js
+++ sites.js
@@ -1,15 +1,15 @@
-let sites = [
-	{
+let sites =
+	[{
 		name: "æstrid",
 		url: 'https://astrid.smith.seattle.wa.us',
 		tagline: "billing excellence is a full time job"
-	},
-	{
+	}
+	,{
 		name: "spacegirl",
 		url: "https://witch.spmh.us",
 		tagline: "i have no idea"
-	},
-	{
+	}
+	,{
 		name: "DashTel",
 		url: "https://dashtel.ca",
 		tagline: "You've reached DashTel, HatbowlDash speaking, how may I direct your call?"
